d9ad406ccf222aa6f842eef70b6ebf4761fd4392
crito <crito@fnordpipe.org>
2018-05-15 21:28:48 +0200

    account: drop send_email_verification

--- src/modules/core/account.lua
+++ src/modules/core/account.lua
@@ -2,9 +2,6 @@ local config = require('core.config')
 local luasql = require('luasql.mysql')
 local salt = require('salt')
 
-local smtp = require("resty.smtp")
-local mime = require("resty.smtp.mime")
-
 local mysql = luasql.mysql()
 local db = mysql:connect(
   config.db.realmd.name,
@@ -35,7 +32,7 @@ end
 function account.create(self, username, email, password, ip)
   local cursor = db:execute(
     "INSERT INTO account \
-      (username, sha_pass_hash, email, joindate, last_ip) \
+      (username, sha_pass_hash, email, email_check, joindate, last_ip) \
       VALUES ( \
         'UPPER(" .. db:escape(username) .. ")', \
         SHA1(CONCAT( \
@@ -43,6 +40,7 @@ function account.create(self, username, email, password, ip)
           UPPER('" .. db:escape(password) .. "') \
         )), \
         'LOWER(" .. db:escape(email) .. ")', \
+        '" .. salt:gen(16) .. "', \
         NOW(), \
         '" .. db:escape(ip) .. "' \
       );")
@@ -89,38 +87,6 @@ function account.username_exists(self, username)
     return row.id
   end
   return false, 'user does not exist'
-end
-
-function account.send_email_verification(self, id, email)
-  local token = salt:gen(16)
-  local cursor = db:execute(
-    "UPDATE account SET email_check = \
-    '" .. token .. "' WHERE id = '" .. db:escape(id) .. "'")
-  if cursor == 1 then
-    local mail = {
-      headers = {
-        subject = mime.ew(
-          'email verification',
-          nil, { charset= 'utf-8' }),
-        ['Content-Transfer-Encoding']= 'BASE64',
-        ['Content-Type']= "text/plain; charset='utf-8'",
-      },
-      body = mime.b64(
-        'please verify your mail address. \
-          goto https://' .. config.verification.url .. '/' .. token)
-    }
-    local ret, err = smtp.send({
-      from = config.verification.from,
-      rcpt = email,
-      user = config.verification.user,
-      password = config.verification.pass,
-      server = config.verification.host,
-      domain = nil,
-      source = smtp.message(mail),
-    })
-    return true
-  end
-  return nil
 end
 
 return account
